59dd64b6e3dd794bcdd16fa6b030d35d5b01b936
Alexander Campo <kovenenphp@gmail.com>
2020-05-20 19:34:12 -0500

    Changed the directory

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -23,7 +23,7 @@
  */
 
 $sysDir  = dirname(__FILE__);
-$rootDir = dirname(dirname(dirname($sysDir)));
+$rootDir = dirname(dirname(dirname(dirname($sysDir))));
 
 // Location to the paths config file
 $config = require $rootDir.DIRECTORY_SEPARATOR.'config'.DIRECTORY_SEPARATOR.'paths.php';
@@ -43,9 +43,9 @@ define('SYS_PATH', realpath($config['path.sys']).DIRECTORY_SEPARATOR);
 require $rootDir.DIRECTORY_SEPARATOR.'config'.DIRECTORY_SEPARATOR.'constants.php';
 
 // Activate the framework class autoloader
-require $sysDir.DIRECTORY_SEPARATOR.'src'.DIRECTORY_SEPARATOR.'classes'.DIRECTORY_SEPARATOR.'Autoloader'.DIRECTORY_SEPARATOR.'Autoloader.php';
+require $sysDir.DIRECTORY_SEPARATOR.'classes'.DIRECTORY_SEPARATOR.'Autoloader'.DIRECTORY_SEPARATOR.'Autoloader.php';
 // Call the class configuration Autoloader
-require $sysDir.DIRECTORY_SEPARATOR.'src'.DIRECTORY_SEPARATOR.'classes'.DIRECTORY_SEPARATOR.'Config'.DIRECTORY_SEPARATOR.'AutoloadConfig.php';
+require $sysDir.DIRECTORY_SEPARATOR.'classes'.DIRECTORY_SEPARATOR.'Config'.DIRECTORY_SEPARATOR.'AutoloadConfig.php';
 
 // Aliases of the class autoloader 
 class_alias('Syscodes\\Autoloader', 'Autoloader');
