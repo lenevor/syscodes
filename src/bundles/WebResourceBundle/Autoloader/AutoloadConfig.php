14dc084b894b496278266c8bdd52dd16e75caecb
Alexander Campo <jalexcam@gmail.com>
2021-11-01 02:34:45 -0500

    Changed namespace and paths of files applied

--- src/bundles/WebResourceBundle/Autoloader/AutoloadConfig.php
+++ src/bundles/WebResourceBundle/Autoloader/AutoloadConfig.php
@@ -20,7 +20,7 @@
  * @license     https://opensource.org/licenses/BSD-3-Clause New BSD license or see https://lenevor.com/license or see /license.md
  */
 
-namespace Syscodes\Bundles\ApplicationBundle\Autoloader;
+namespace Syscodes\Bundles\WebResourceBundle\Autoloader;
 
 /**
  * Auto-loader Config
@@ -42,7 +42,7 @@ abstract class AutoloadConfig
 	 *  
 	 * @var string[] $coreClassmap
 	 */
-	protected $coreClassmap = SYS_PATH.'src'.DIRECTORY_SEPARATOR.'register'.DIRECTORY_SEPARATOR.'autoloadClassmap.php';
+	protected $coreClassmap = __DIR__.DIRECTORY_SEPARATOR.'register'.DIRECTORY_SEPARATOR.'autoloadClassmap.php';
 
 	/**
 	 * ---------------------------------------------------------------------
@@ -54,7 +54,7 @@ abstract class AutoloadConfig
 	 * 
 	 * @var string[] $coreFiles
 	 */
-	protected $coreFiles = SYS_PATH.'src'.DIRECTORY_SEPARATOR.'register'.DIRECTORY_SEPARATOR.'autoloadFiles.php';
+	protected $coreFiles = __DIR__.DIRECTORY_SEPARATOR.'register'.DIRECTORY_SEPARATOR.'autoloadFiles.php';
 
 	/**
 	 * ---------------------------------------------------------------------
@@ -67,5 +67,5 @@ abstract class AutoloadConfig
 	 * 
 	 * @var string[] $corePsr4
 	 */
-	protected $corePsr4 = SYS_PATH.'src'.DIRECTORY_SEPARATOR.'register'.DIRECTORY_SEPARATOR.'autoloadPsr4.php';
+	protected $corePsr4 = __DIR__.DIRECTORY_SEPARATOR.'register'.DIRECTORY_SEPARATOR.'autoloadPsr4.php';
 }
